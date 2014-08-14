49036e9369179b7f0834b5ba579ff822043f07b2
Zishan Ahmad <zishanahmad@outlook.com>
2014-08-15 00:16:19 +0530

    More styling to the landing page

--- application/views/landing_layout.php
+++ application/views/landing_layout.php
@@ -17,10 +17,21 @@
     		font-size: 20px;
     	}
     	input {
-    		border: 0;
+    		-webkit-transition: all 0.30s ease-in-out;
+			-moz-transition: all 0.30s ease-in-out;
+			-ms-transition: all 0.30s ease-in-out;
+			-o-transition: all 0.30s ease-in-out;
+			border: 0;
     		padding: 10px;
     		margin-left: 5px;
+			outline: none;
+			border: 0px solid #DDDDDD;
     	}
+    	input:focus {
+		    abox-shadow: 0 0 9px rgba(81, 203, 238, 1);
+		    box-shadow: 0 0 5px 1.50px rgba(255,0,0,1);
+		    border: 0px solid rgba(81, 203, 238, 1);
+		}
     	input[type="submit"] {
     		background: red;
     		color: white;
@@ -39,7 +50,7 @@
 		<h1>Welcome to Jamia Connect</h1>
 		<p>A place to get in touch, collaborate and share with JMI'tes</p>
 		<form>
-			<input type="email" placeholder="Email" name="email">
+			<input type="email" placeholder="Email" name="email" autofocus>
 			<input type="password" placeholder="Password" name="password">
 			<input type="submit" name="submit" value="Login">
 		</form>
