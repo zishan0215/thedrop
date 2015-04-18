3bc463ff7b55d4e68d9013d4228f01deb6c694b3
Zishan Ahmad <zishanahmad@outlook.com>
2015-04-18 15:46:26 +0530

    changed theme

---
+++ application/views/world/world_header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>The Drop</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="/thedrop/assets/js/jquery.min.js"></script>
+		<script src="/thedrop/assets/js/jquery.dropotron.min.js"></script>
+		<script src="/thedrop/assets/js/skel.min.js"></script>
+		<script src="/thedrop/assets/js/skel-layers.min.js"></script>
+		<script src="/thedrop/assets/js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="/thedrop/assets/css/skel.css" />
+			<link rel="stylesheet" href="/thedrop/assets/css/style.css" />
+			<link rel="stylesheet" href="/thedrop/assets/css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
